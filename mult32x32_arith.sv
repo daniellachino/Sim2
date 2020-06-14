de7ca8999ce17b220cf923ec2e25605c40ed4638
daniellachino <daniel.lachino+github@gmail.com>
2020-06-14 17:38:29 +0300

    mult32x32_arith upload No.1

--- mult32x32_arith.sv
+++ mult32x32_arith.sv
@@ -14,7 +14,32 @@ module mult32x32_arith (
 
 // Put your code here
 // ------------------
+logic [8:0] mx_1_out;
+logic [16:0] mx_2_out;
+logic [24:0] mult_out;
+logic [64:0] shifter_out;
+logic [64:0] adder_out;
+always_ff @(posedge clk, posedge reset) begin
+    if (upd_prod) begin
+        product <= adder_out;
+    end else begin
+        product <=0;
+    end
+    if (clr_prod) product <= 0;
+end
 
+always_comb begin
+    mx_1_out = a[8*a_sel:8*(a_sel+1)];
+    mx_2_out = a[16*b_sel:16*(b_sel+1)];
+    mult_out = mx_1_out * mx_2_out;
+    if (shift_sel <=5) begin 
+    shifter_out = mult_out<<( 8* (2**shift_sel));
+    end else begin
+        shifter_out = 0;
+    end
+    adder_out = shifter_out+product;
+    
+end
 
 // End of your code
 
