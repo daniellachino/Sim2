6e9920f40bdb340e7a5f64580421299942921fce
daniellachino <daniel.lachino+github@gmail.com>
2020-06-14 19:21:33 +0300

    mult32x32 and its test **has bugs**

--- mult32x32_test.sv
+++ mult32x32_test.sv
@@ -11,7 +11,32 @@ module mult32x32_test;
 
 // Put your code here
 // ------------------
+mult32x32 UUT(  clk,            // Clock
+                reset,          // Reset
+                start,          // Start signal
+                a,       // Input a
+                b,       // Input b
+                busy,          // Multiplier busy indication
+                 product // Miltiplication product
+);
 
+always begin
+    #5 clk = ~clk;
+end
+
+initial begin
+    clk = 0;
+    reset = 1;
+    start = 0;
+    #20;
+    reset = 0;
+    a = 314968744;
+    b = 208081125;
+    #5;
+    start = 1;
+    #5;   
+
+end
 // End of your code
 
 endmodule
